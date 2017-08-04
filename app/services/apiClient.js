f1dcefe69b330028fd008eb35935c5fd0927bce4
Ben Hodgson <ben@someaddress.net>
2017-08-04 15:24:28 +0100

    Use new API endpoint path format

--- app/services/apiClient.js
+++ app/services/apiClient.js
@@ -3,7 +3,7 @@ import * as config from '../constants/config'
 
 const apiClient = {
   get(endpoint) {
-    return fetch(`${config.API_ENDPOINT}/?get=${encodeURIComponent(endpoint)}`, {
+    return fetch(`${config.API_ENDPOINT}${endpoint}`, {
       method: 'get'
     })
   },
@@ -11,11 +11,11 @@ const apiClient = {
   artistSearch(searchQuery) {
     let artistData
     return new Promise((resolve, reject) => {
-      this.get(`search/artists.json?query=${searchQuery}`)
+      this.get(`/search/artists.json?query=${searchQuery}`)
       .then(r => r.json())
       .then((data) => {
         artistData = data.resultsPage.results.artist[0]
-        return this.get(`artists/${artistData.id}/calendar.json`)
+        return this.get(`/artists/${artistData.id}/calendar.json`)
       })
       .then(r => r.json())
       .then((data) => {
